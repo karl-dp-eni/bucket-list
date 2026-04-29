d57d84e4d7ce4ff20e3ca091b60f0c160c42b1b7
karl-dp-eni <karl.dupuy2025@campus-eni.fr>
2026-04-29 12:29:44 +0200

    Added database connection and QueryBuilder

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\WishRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,16 +11,26 @@ use Symfony\Component\Routing\Attribute\Route;
 final class WishController extends AbstractController
 {
     #[Route('', name: 'list')]
-    public function list(): Response
+    public function list(WishRepository $wishRepository): Response
     {
-        // TODO : Afficher la liste des souhaits
-        return $this->render('wish/list.html.twig');
+        $wishes = $wishRepository->findMostRecentWishes();
+
+        return $this->render('wish/list.html.twig', [
+            'wishes' => $wishes
+        ]);
     }
 
     #[Route('/{id}', name: 'detail', requirements: ['id' => '\d+'], methods: ['GET'])]
-    public function detail(int $id): Response
+    public function detail(int $id, WishRepository $wishRepository): Response
     {
-        // TODO : Afficher le détail d'un souhait
-        return $this->render('wish/detail.html.twig');
+        $wish = $wishRepository->find($id);
+
+        if (!$wish) {
+            throw $this->createNotFoundException("Ooops ! Ce souhait n'existe pas !");
+        }
+
+        return $this->render('wish/detail.html.twig', [
+            'wish' => $wish
+        ]);
     }
 }
